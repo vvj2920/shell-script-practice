f893aca3c97891ca5fc01e9145e74abd8caf4c3b
jagadeesh <vvjagadeesh07@gmail.com>
2024-12-06 21:04:36 +0530

    02-variable.sh added

---
+++ 02-variable.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+PERSON1=Jagadeesh
+PERSON2="Jithendra
+
+echo "$PERSON1 is elder brother of $PERSON2"
+
+var1=Rant
+var2=stand
+
+echo "$Rant with $stand"
+
+unset rant
+
+echo "$Rant with $stand"
+
+echo "Enter the value of A :"
+read a
+echo "Enter  the value of B :"
+read b
+
+echo "Sum of value is ( $a+$b)"
+
+name1=jagdeesh
+name2=vardhineni
+read only  name1
+
+echo  "first name  $name1 ,last name $name2"
+
+name1=Jithendra
+
+echo  "first name  $name1 ,last name $name2"
+
+
+
+
--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 What is #!/bin/bash ?
 A)The shebang tells the terminal to use bash to execute the script
        to know default shell we use command echo $SHELL
+
+echo ---> is a command used to print the statement 
+
+read----->is a command used to take data from user and store it in a variable.
+
+unset--->The unset command directs a shell to delete a variable and its stored data from list of variables
+
+read only--->values could not be modified later in the script.
