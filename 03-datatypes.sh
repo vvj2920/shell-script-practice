97333487d043fed21ac0d9d982f136cd3683de63
jagadeesh <vvjagadeesh07@gmail.com>
2024-12-07 19:21:41 +0530

    datatype.sh  file created

--- 03-datatypes.sh
+++ 03-datatypes.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
-NUMBER1=700
-NUMBER2=800
+NUMBER1=$1
+NUMBER2=$2
 SUM=$(($NUMBER1+$NUMBER2))
 
 echo "TOTAL :: $SUM "
+echo "How many args passed:: $#"
+
+echo "All args passed:: $@"
+
+echo "Script name:: $0"
