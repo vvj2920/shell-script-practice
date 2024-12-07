75055d2759569d391af9ec66fcc6ab382292e733
jagadeesh <vvjagadeesh07@gmail.com>
2024-12-07 18:58:48 +0530

    Commited

--- 02-variable.sh
+++ 02-variable.sh
@@ -4,21 +4,7 @@ PERSON2="Jithendra
 
 echo "$PERSON1 is elder brother of $PERSON2"
 
-var1=Rant
-var2=stand
 
-echo "$Rant with $stand"
-
-unset rant
-
-echo "$Rant with $stand"
-
-echo "Enter the value of A :"
-read a
-echo "Enter  the value of B :"
-read b
-
-echo "Sum of value is  $a+$b"
 
 
 
