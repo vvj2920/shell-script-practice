63977e4fdf1c98cf0eb84866f4b67587b6a34e1c
jagadeesh <vvjagadeesh07@gmail.com>
2024-12-07 19:09:43 +0530

    date added in variable.sh

--- 02-variable.sh
+++ 02-variable.sh
@@ -5,7 +5,12 @@ date=$(date)
 echo "Script started at ${date}"
 echo " $PERSON1 is elder brother of $PERSON2 "
 
+echo "Please enter your username ::"
+read username
 
+echo "Password enter"
+read password
+ echo "username is $username and password is $password
 
 
 
