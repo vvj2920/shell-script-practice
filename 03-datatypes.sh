24f13ee277d6a5acf2b9a83ec3a72b6ad56df632
jagadeesh <vvjagadeesh07@gmail.com>
2024-12-12 09:40:16 +0530

    comitted

--- 03-datatypes.sh
+++ 03-datatypes.sh
@@ -2,9 +2,13 @@
 
 NUMBER1=$1
 NUMBER2=$2
+
 SUM=$(($NUMBER1+$NUMBER2))
 
-echo "TOTAL :: $SUM"
+echo "Total:: $SUM"
+
 echo "How many args passed:: $#"
+
 echo "All args passed:: $@"
+
 echo "Script name:: $0"
